fabdb2e2826f3adf680c4d30b02ea126eebf0df4
Gabriel Zavando <gabrielzavando@gmail.com>
2018-06-29 08:56:13 -0400

    añadí condicional para mostrar logo por default y modifiqué el menu

--- header.php
+++ header.php
@@ -35,7 +35,14 @@
 		?>
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
+			if(has_custom_logo()){
+				the_custom_logo();
+			}else{
+				?>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" />
+				<?php
+			}
+				
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -53,7 +60,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'modelo-completo' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'modelo-completo' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
